bcb45444430ad7e9e4a59f5cdc3d4a7bc855ea09
yngver <yngve.refseth@uv.uio.no>
2016-03-08 12:36:18 +0100

    accordion adjusted

---
+++ relekvantscript.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yngver on 08.03.2016.
+ */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
